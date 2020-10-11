36430c1add2801b43307299e189f8d15218e33f6
magalierambla <magalierambla@users.noreply.github.com>
2020-10-11 18:44:48 +0200

    remove security tmp

--- src/main/java/com/api/crowdlending/repository/ProjectRepository.java
+++ src/main/java/com/api/crowdlending/repository/ProjectRepository.java
@@ -4,12 +4,14 @@ import com.api.crowdlending.model.ProjectCategory;
 import com.api.crowdlending.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RepositoryRestResource(path = "projects")
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 	 @Query(value = "SELECT  *  FROM  projects  WHERE  token_user=?1", nativeQuery = true)
 	 List<Project> findAllProjectByToken(String tokenUser);
