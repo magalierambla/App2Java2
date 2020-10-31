a7563197483f731018c8e7070c172799ae7ec6ca
magalierambla <magalierambla@users.noreply.github.com>
2020-10-31 18:56:52 +0100

    open only root

--- src/main/java/com/api/crowdlending/auth/ApplicationSecurityConfiguration.java
+++ src/main/java/com/api/crowdlending/auth/ApplicationSecurityConfiguration.java
@@ -43,7 +43,7 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**")
+                .antMatchers("/")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
